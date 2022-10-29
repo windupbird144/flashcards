687612df156932e0cad0d454670e39cd03d7bfb7
Diyar Oktay <diyar.oktay@outlook.com>
2022-10-30 00:39:29 +0200

    Show About page instead of blank page on landing

--- src/app.tsx
+++ src/app.tsx
@@ -28,15 +28,14 @@ function Page(props: { hash: string }) {
   if (props.hash === "#study") return <PageStudy></PageStudy>;
   if (props.hash === "#add-card") return <PageAddCard></PageAddCard>;
   if (props.hash === "#browse") return <PageBrowse></PageBrowse>;
-  if (props.hash === "#about") return <PageAbout></PageAbout>;
-  return <></>;
+  return <PageAbout></PageAbout>;
 }
 
 function PageStudy() {
   // Fetch the database connection
   const [db, setDb] = useState<Database | null>(null);
   const [card, setCard] = useState<Card | null>(null);
-  
+
   useEffect(() => {
     getDatabase().then((db) => {
       setDb(db);
@@ -47,64 +46,67 @@ function PageStudy() {
   if (!db) return <></>;
 
   function judge(e: Event) {
-    const target = e?.target as HTMLElement
-    if (!target) return
-    if (!card) return
-    const choice = target.dataset.value
-    console.log(choice)
-    if (!choice) return
-    const  remembered = choice === "yes"
-    const newCard = judgeCard(card, remembered)
+    const target = e?.target as HTMLElement;
+    if (!target) return;
+    if (!card) return;
+    const choice = target.dataset.value;
+    console.log(choice);
+    if (!choice) return;
+    const remembered = choice === "yes";
+    const newCard = judgeCard(card, remembered);
     // update the card
-    db?.updateCard(newCard)
+    db?.updateCard(newCard);
     // load the next card to study
-    db?.getNextStudyCard().then(setCard)
+    db?.getNextStudyCard().then(setCard);
   }
-
-  return <>
-      <h3>Study</h3>
-      {
-        (!card)
-          ? <p>There are no cards to study right now.</p>
-          : <PageStudyCard
-          card={card}
-          onJudge={judge}
-          />
-      }
-  </>
-  
-}
-
-function PageStudyCard(props: {
-    card: Card
-    onJudge: (e: Event) => void
-  }) {
-
-  const [hidden, setHidden] = useState(true);
-  const reveal = () => setHidden(false)
-
-  // hide when the card changes
-  useEffect(() => { setHidden(true)}, [props.card])
 
   return (
     <>
-      {props.card === null && <p>"There are no cards to study at this time.";</p>}
+      <h3>Study</h3>
+      {!card ? (
+        <p>There are no cards to study right now.</p>
+      ) : (
+        <PageStudyCard card={card} onJudge={judge} />
+      )}
+    </>
+  );
+}
 
-      <div>
-        { props.card?.front }
-        {
-          hidden
-            ? <div class="cover" onClick={reveal}>Click to reveal</div>
-            : <div class="card-back">
-              <div>{props.card?.back}</div>
-              <div className="did-you-remember">Did you remember?</div>
-              <div className="buttons" onClick={props.onJudge}>
-                <button type="submit" data-value="yes">yes</button>
-                <button type="submit" data-value="no">no</button>
-              </div>
+function PageStudyCard(props: { card: Card; onJudge: (e: Event) => void }) {
+  const [hidden, setHidden] = useState(true);
+  const reveal = () => setHidden(false);
 
+  // hide when the card changes
+  useEffect(() => {
+    setHidden(true);
+  }, [props.card]);
+
+  return (
+    <>
+      {props.card === null && (
+        <p>"There are no cards to study at this time.";</p>
+      )}
+
+      <div class="card">
+        <div className="card-front">{props.card?.front}</div>
+        {hidden ? (
+          <div class="card-cover" onClick={reveal}>
+            Click to reveal
+          </div>
+        ) : (
+          <div class="card-back">
+            <div>{props.card?.back}</div>
+            <div className="did-you-remember">Did you remember?</div>
+            <div className="buttons" onClick={props.onJudge}>
+              <button type="submit" data-value="yes">
+                yes
+              </button>
+              <button type="submit" data-value="no">
+                no
+              </button>
             </div>
-        }
+          </div>
+        )}
       </div>
     </>
   );
@@ -115,13 +117,29 @@ function PageAbout() {
     <article>
       <h3>About Flashcards</h3>
       <p>Offline flash cards app for the browser</p>
-      <p>Flashcards is an app to create and study flash cards. It works in the browser as an offline app. The app is very simple. For more advanced use cases, see [Wikipedia: List of flash card software](https://en.wikipedia.org/wiki/List_of_flashcard_software) for good solutions.</p>
+      <p>
+        Flashcards is an app to create and study flash cards. It works in the
+        browser as an offline app. The app is very simple. For more advanced use
+        cases, see [Wikipedia: List of flash card
+        software](https://en.wikipedia.org/wiki/List_of_flashcard_software) for
+        good solutions.
+      </p>
       <h4>Flash cards</h4>
-      <p>The app lets you create flash cards with a front (text) and back (text). In a study session, you review each card and note whether you remembered the answer or not. Cards which you remember will appear less frequently while cards you failed to remember appear sooner. This method is called [spaced repitition](https://en.wikipedia.org/wiki/Spaced_repetition).</p>
+      <p>
+        The app lets you create flash cards with a front (text) and back (text).
+        In a study session, you review each card and note whether you remembered
+        the answer or not. Cards which you remember will appear less frequently
+        while cards you failed to remember appear sooner. This method is called
+        [spaced repitition](https://en.wikipedia.org/wiki/Spaced_repetition).
+      </p>
 
-    <h4>Work offline</h4>
-    <p>The app works offline. Open the app in your browser and select "Add to home screen". This adds an icon to your home screen and makes the app available offline.</p>
-  </article>
+      <h4>Work offline</h4>
+      <p>
+        The app works offline. Open the app in your browser and select "Add to
+        home screen". This adds an icon to your home screen and makes the app
+        available offline.
+      </p>
+    </article>
   );
 }
 
@@ -155,10 +173,8 @@ function PageBrowse() {
   return (
     <>
       <h3>Browse</h3>
-      <p hidden={!!cards.length}>
-        You have not created any cards.
-      </p>
-      <table hidden={cards.length<1}>
+      <p hidden={!!cards.length}>You have not created any cards.</p>
+      <table hidden={cards.length < 1}>
         <thead>
           <tr>
             <th>Front</th>
@@ -207,26 +223,28 @@ function PageAddCard() {
     <>
       <h3>Add card</h3>
       <form id="form-add-card" onSubmit={onsubmit}>
-        <label htmlFor="front">Front</label>
-        <textarea
-          required
-          name="front"
-          id="front"
-          rows={5}
-          value={front}
-          onInput={(e) => setFront(e.currentTarget.value ?? "")}
-        ></textarea>
-
-        <label htmlFor="back">Back</label>
-        <textarea
-          required
-          name="back"
-          id="back"
-          rows={5}
-          value={back}
-          onInput={(e) => setBack(e.currentTarget.value ?? "")}
-        ></textarea>
-
+        <div className="front">
+          <label htmlFor="front">Front</label>
+          <textarea
+            required
+            name="front"
+            id="front"
+            rows={5}
+            value={front}
+            onInput={(e) => setFront(e.currentTarget.value ?? "")}
+          ></textarea>
+        </div>
+        <div className="back">
+          <label htmlFor="back">Back</label>
+          <textarea
+            required
+            name="back"
+            id="back"
+            rows={5}
+            value={back}
+            onInput={(e) => setBack(e.currentTarget.value ?? "")}
+          ></textarea>
+        </div>
         <button type="submit">Add</button>
       </form>
     </>
